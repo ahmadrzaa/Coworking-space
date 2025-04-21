85e9f309088cfe91b4bddeebff513dfc8984cada
ahmadrzaa <siddiquea725@gmail.com>
2025-04-21 10:26:35 +0300

    Deploy build to gh-pages

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+// ------------------------------------------------------------
+// © 2025 Ahmad Raza  (@ahmadrzaa on GitHub)
+// All rights reserved. Do not distribute without permission.
+// ------------------------------------------------------------
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: "/Coworking-space/",
+  plugins: [react()]
+})
